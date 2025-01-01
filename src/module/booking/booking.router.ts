d8c37a7a2bdd50417f1459e11f57408c76f2557a
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-01-01 17:39:51 +0600

    Commented code deleted done

--- src/module/booking/booking.router.ts
+++ src/module/booking/booking.router.ts
@@ -1,23 +1,3 @@
-// // create booking
-// // get all booking
-// // get booking by id
-// // get booking by user id = myBookings
-// // update booking
-// // delete booking = soft.delete
-
-// import express, { Request, Response } from 'express'
-// import { bookingController } from './booking.controller'
-// export const bookingRouter = express.Router()
-
-// bookingRouter.post('/create-booking', bookingController.createBooking)
-
-// create booking
-// get all booking
-// get booking by id
-// get booking by user id = myBookings
-// update booking
-// delete booking = soft delete
-
 import express from 'express'
 import { bookingController } from './booking.controller'
 
