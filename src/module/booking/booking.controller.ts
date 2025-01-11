33519375fd8d5718642058fb26daabbc5f77fdf6
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-01-11 19:38:11 +0600

    Updated booking file

--- src/module/booking/booking.controller.ts
+++ src/module/booking/booking.controller.ts
@@ -3,23 +3,6 @@ import { sendResponse } from '../../utils/sendResponse'
 import { BookingService } from './booking.service'
 import { catchAsync } from '../../utils/catchAsync'
 
-// const createBooking = catchAsync(async (req, res) => {
-//   const payload = req.body
-
-//   const result = await BookingService.createBooking(payload)
-
-//   // use utils --> sendResponse.ts file for use hooks/short code
-//   sendResponse(res, {
-//     statusCode: StatusCodes.CREATED,
-//     message: 'Booking created successfully.',
-//     data: result,
-//   })
-// })
-
-// export const bookingController = {
-//   createBooking,
-// }
-
 const createBooking = catchAsync(async (req, res) => {
   const body = req.body
   const result = await BookingService.createBooking(body)
